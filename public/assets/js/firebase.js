54212a233e895f299ffb2c0ffccd7a3736cf825f
vlcolquett <vlcolquett@gmail.com>
2023-06-04 11:30:31 -0600

    pulls from firestore not storage; added inputs

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js'
-import { getStorage, ref, listAll, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-storage.js"			
+//import { getStorage, ref, list, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-storage.js"			
 // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js'
 
 // Add Firebase products that you want to use
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
 
 console.log("boo");
 
@@ -19,77 +19,27 @@ var firebaseConfig = {
   measurementId: "G-9HLKY9YBQF"
 };
 const firebase = initializeApp(firebaseConfig);
+//################################ FIRESTORE
+const db = getFirestore(firebase);
 
-// Create a reference to the "flag" folder
-// Get a reference to the storage service, which is used to create references in your storage bucket
-const storage = getStorage();
+const colRef = collection(db, 'country_flags');
+
+const docsSnap = await getDocs(colRef);
+
+
 var check
 var anchorElement
 var articleElement
 var imageContainer = document.getElementById('main');
-// Create a storage reference from our storage service
-const storageRef = ref(storage, 'country_flags');
-console.log("hello");
-// Fetch the list of images in the 'flag' folder
-// listAll(storageRef)
-//   .then(function(result) {
-//     result.items.forEach(function(imageRef) {
-//       // Get the download URL for each image
-//       getDownloadURL(imageRef).then(function(url) {
-//         // Create an <img> element
-//         var imageElement = document.createElement('img');
-//         imageElement.src = url;
-//         imageElement.alt = '';
-
-//         // Create an <input> element for the country name
-//         var countryInput = document.createElement('input');
-//         countryInput.type = 'text';
-//         countryInput.name = 'country';
-//         countryInput.id = 'country';
-//         countryInput.value = '';
-//         countryInput.style.width = '50%';
-//         countryInput.style.bottom = '1em';
-//         countryInput.style.left = '1em';
-//         countryInput.style.position = 'absolute';
-//         countryInput.style.borderRadius = '5px';
-//         countryInput.style.opacity = '.5';
-
-//         // Create an <input> element for the guess button
-//         var guessButton = document.createElement('input');
-//         guessButton.type = 'submit';
-//         guessButton.value = 'guess';
-//         guessButton.style.bottom = '1em';
-//         guessButton.style.right = '1em';
-//         guessButton.style.position = 'absolute';
-//         guessButton.style.borderRadius = '5px';
-//         guessButton.style.backgroundColor = '#34363b';
-//         guessButton.style.opacity = '.5';
-//         guessButton.className = 'primary';
-
-//         // Create a <a> element
-//         var imageLink = document.createElement('a');
-//         imageLink.className = 'image';
-//         imageLink.appendChild(imageElement);
-//         imageLink.appendChild(countryInput);
-//         imageLink.appendChild(guessButton);
-
-//         // Append the <a> element to the image container
-//         imageContainer.appendChild(imageLink);
-//       }).catch(function(error) {
-//         console.log('Error getting download URL:', error);
-//       });
-//     });
-//   })
-//   .catch(function(error) {
-//     console.log('Error fetching images:', error);
-//   });
-//Get the list of images in the "flag" folder
-  listAll(storageRef).then(function(result) {
-  result.items.forEach(function(imageRef) {
-    // Get the download URL for each image
-    getDownloadURL(imageRef).then(function(url) {
-      // Display the image on your webpage
-      const img = document.createElement('img');
+var i = 0;
+docsSnap.forEach(doc => {
+  // get the id
+  //console.log(doc.id);
+  //console.log(doc.data()['name'], doc.data()['url']);
+  i = i +1;
+  console.log(i);
+  const url = doc.data()['url'];
+  const img = document.createElement('img');
       img.src = url;
       img.style.backgroundPosition = 'center';
           img.style.backgroundRepeat = 'no-repeat';
@@ -99,11 +49,37 @@ console.log("hello");
           img.style.height = '95%';
           img.style.width = '95%';
           img.style.position = 'absolute';
-          //img.style.top = '0';
-          //img.style.left = '0';
 
-      anchorElement = document.createElement('a');
+                  // Create an <input> element for the country name
+                  var countryInput = document.createElement('input');
+                  countryInput.type = 'text';
+                  countryInput.name = 'country';
+                  countryInput.id = i.toString();
+                  countryInput.value = '';
+                  countryInput.style.width = '50%';
+                  countryInput.style.bottom = '3em';
+                  countryInput.style.left = '1em';
+                  countryInput.style.position = 'absolute';
+                  countryInput.style.borderRadius = '5px';
+                  countryInput.style.opacity = '.5';
+          
+                  // Create an <input> element for the guess button
+                  var guessButton = document.createElement('input');
+                  guessButton.type = 'submit';
+                  guessButton.value = 'guess';
+                  guessButton.id = i.toString();
+                  guessButton.style.bottom = '3em';
+                  guessButton.style.right = '3em';
+                  guessButton.style.position = 'absolute';
+                  guessButton.style.borderRadius = '5px';
+                  guessButton.style.backgroundColor = '#34363b';
+                  guessButton.style.opacity = '.5';
+                  guessButton.className = 'primary';
+
+          anchorElement = document.createElement('a');
           anchorElement.appendChild(img);
+          anchorElement.appendChild(countryInput);
+          anchorElement.appendChild(guessButton);
          anchorElement.classList.add('image');
           anchorElement.style.backgroundSize = 'cover';
           anchorElement.style.content = '';
@@ -122,21 +98,47 @@ console.log("hello");
           articleElement.classList.add('thumb');  
           articleElement.appendChild(anchorElement);
 
-      imageContainer = document.getElementById('main');
+      //var imageContainer = document.getElementById('main');
           imageContainer.appendChild(articleElement);      
-          check = imageContainer.innerHTML
+          //check = imageContainer.innerHTML
+
+
+})
+
+//######################### STORAGE ############################################
+// Create a reference to the "flag" folder
+// Get a reference to the storage service, which is used to create references in your storage bucket
+// const storage = getStorage();
+
+
+// // Create a storage reference from our storage service
+// const storageRef = ref(storage, 'country_flags');
+// console.log("hello");
+
+// //Get the list of images in the "flag" folder
+//   list(storageRef).then(function(result) {
+//   result.items.forEach(function(imageRef) {
+//     // Get the download URL for each image
+//     getDownloadURL(imageRef).then(function(url) {
+//       // Display the image on your webpage
+//       //console.log(url);
+      
+//           //img.style.top = '0';
+//           //img.style.left = '0';
+
+
 
           
-          //document.body.appendChild(img);
-    }).catch((error) => {
-      // Handle any errors
-      console.log(error);
-    });
-  });
-}).catch((error) => {
-  // Handle any errors
-  console.log(error);
-});
+//           //document.body.appendChild(img);
+//     }).catch((error) => {
+//       // Handle any errors
+//       console.log(error);
+//     });
+//   });
+// }).catch((error) => {
+//   // Handle any errors
+//   console.log(error);
+// });
 
 document.addEventListener("DOMContentLoaded", function() {
   const images = document.querySelectorAll('a');
@@ -147,3 +149,34 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+
+// Fetch the list of images in the 'flag' folder
+// listAll(storageRef)
+//   .then(function(result) {
+//     result.items.forEach(function(imageRef) {
+//       // Get the download URL for each image
+//       getDownloadURL(imageRef).then(function(url) {
+//         // Create an <img> element
+//         var imageElement = document.createElement('img');
+//         imageElement.src = url;
+//         imageElement.alt = '';
+
+
+
+//         // Create a <a> element
+//         var imageLink = document.createElement('a');
+//         imageLink.className = 'image';
+//         imageLink.appendChild(imageElement);
+
+
+//         // Append the <a> element to the image container
+//         imageContainer.appendChild(imageLink);
+//       }).catch(function(error) {
+//         console.log('Error getting download URL:', error);
+//       });
+//     });
+//   })
+//   .catch(function(error) {
+//     console.log('Error fetching images:', error);
+//   });
